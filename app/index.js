7c289b3104cf9276704cea90f3064b1e5aaede90
YFAR02 <yfarooqui001@gmail.com>
2025-07-05 16:46:45 -0500

    Initial commit

---
+++ app/index.js
@@ -0,0 +1,9 @@
+import { View, Text} from 'react-native'; //view is like div and text is like a regular p tag
+
+const Home = () => {
+    return ( 
+        <View> </View>
+    )
+}
+
+export default Home; //exporting the component so it can be used in other files
